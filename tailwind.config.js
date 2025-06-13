97ae0f7518e6d9f8406c440b93f718437c40a7fa
XxtheTianxX <sebastiangomezpe@gmail.com>
2025-06-12 19:32:01 -0500

    v2.1.3

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class', // 👈 habilita modo oscuro por clase
+  content: ['./index.html', './script.js'], // Ajusta según tus archivos reales
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
